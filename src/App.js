37955c95e4731c2312fad11d8af686f945ae8653
infomagnum159 <infomagnum159@gmail.com>
2022-06-06 18:34:40 +0600

    Connected all files to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import GamingField from './components/Gaming Field';
+import Counter from './components/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+
+    return (
+        <div className="App">
+            <h1>Minesweeper</h1>
+            <GamingField />
+            <Counter />
+        </div>
+    );
+};
 
 export default App;
